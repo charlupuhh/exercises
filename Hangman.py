dbab1cb0514e2d7d4ebc9f2a1b0668f821a510cc
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-04-08 18:30:57 -0700

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,109 @@
+from random_word import RandomWords
+
+r = RandomWords()
+word = r.get_random_word(hasDictionaryDef="true") #create word
+while "-" in word: #make sure word doesn't have '-'
+    word = r.get_random_word(hasDictionaryDef="true")
+print(word)
+gallows = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
+
+guessed_wrong = 0
+guessed_word = []
+guessed_letters = []
+game_over = False
+word_range = (range(len(word)))
+for i in word_range:
+    guessed_word.append('_')
+
+def gameCheck(): #check if the game is over
+    global game_over
+    if guessed_wrong == 6:
+        print("You have too many incorrect guesses, the word was % s better luck next time"% word)
+        game_over = True
+    elif '_' not in guessed_word:
+        print("You have guessed the word, congratulations!")
+        print(word)
+        game_over = True
+
+print("Welcome to Hangman, you will have 6 guesses to try and guess the word. You may either guess a letter or the whole word")
+print("Here is your starting board")
+
+while game_over == False:
+    print(gallows[guessed_wrong])
+    print(str(guessed_wrong) + "/6 wrong")
+    print(*guessed_word)
+    print("So far you have guessed %s"% guessed_letters)
+    print("What would you like to guess")
+    guess = input()
+    if len(guess) == len(word):
+        if guess == word:
+            print("You have guessed the word, congratulations!")
+            game_over = True
+        else:
+            print("That was not the word")
+            gameCheck()
+    elif len(guess) != 1 & len(guess) != len(word):
+            print("Please guess either a letter or a word(of the same length as the word), try again.")
+    elif guess in guessed_letters:
+            print("You have already guessed that, please try again.")
+    else:
+        guessed_letters.append(guess)
+        if guess not in word:
+            guessed_wrong += 1
+            gameCheck()
+        else:
+            for i in word_range:
+                if guess == word[i]:
+                    guessed_word[i] = guess
+            gameCheck()
